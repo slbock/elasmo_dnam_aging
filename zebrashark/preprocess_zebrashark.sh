ef6565022a0bbeaa381175df0c95dac1f7904142
samanthabock <samantha.bock@uga.edu>
2026-03-29 13:43:54 -0400

    update

--- zebrashark/preprocess_zebrashark.sh
+++ zebrashark/preprocess_zebrashark.sh
@@ -9,12 +9,12 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --time=12:00:00
-#SBATCH --output=/scratch/sb61937/scripts/fastqc_zebrashark_5.0.o
-#SBATCH --error=/scratch/sb61937/scripts/fastqc_zebrashark_5.0.e
+#SBATCH --output=/work/dir/scripts/fastqc_zebrashark_5.0.o
+#SBATCH --error=/work/dir/scripts/fastqc_zebrashark_5.0.e
 
 
 ## go to directory with raw reads
-cd /scratch/sb61937/work/ZEBRA_SHARK/RAW_READS/METHYLSEQ_121923
+cd /work/dir/work/ZEBRA_SHARK/RAW_READS/METHYLSEQ_121923
 
 ## load fastqc
 module load FastQC/0.11.9-Java-11
@@ -22,13 +22,13 @@ module load MultiQC/1.14-foss-2022a
 
 for i in GN22895 GN22896 GN22897 GN22898 GN22899 GN22900 GN22901 GN22902 GN22903 GN22904 GN22905 GN22906 GN22907 GN22908 GN22909 GN22910 GN22911 GN22912 GN22913 GN22914 GN22915 GN22916 GN22917 GN22918 GN22919 GN22920 GN22921 GN22922 GN22923 GN22924 GN22925 GN22926 GN22927 GN22928
 do
-  fastqc -t 8 ${i}_*_L00*_R1_001.fastq.gz -o /scratch/sb61937/work/ZEBRA_SHARK/Pretrim_fastqc/METHYLSEQ_121923
-  fastqc -t 8 ${i}_*_L00*_R2_001.fastq.gz -o /scratch/sb61937/work/ZEBRA_SHARK/Pretrim_fastqc/METHYLSEQ_121923
+  fastqc -t 8 ${i}_*_L00*_R1_001.fastq.gz -o /work/dir/work/ZEBRA_SHARK/Pretrim_fastqc/METHYLSEQ_121923
+  fastqc -t 8 ${i}_*_L00*_R2_001.fastq.gz -o /work/dir/work/ZEBRA_SHARK/Pretrim_fastqc/METHYLSEQ_121923
 done
 
 
-cd /scratch/sb61937/work/ZEBRA_SHARK/Pretrim_fastqc/METHYLSEQ_121923
-multiqc -d *_fastqc.zip -o /scratch/sb61937/work/ZEBRA_SHARK/Pretrim_fastqc/METHYLSEQ_121923
+cd /work/dir/work/ZEBRA_SHARK/Pretrim_fastqc/METHYLSEQ_121923
+multiqc -d *_fastqc.zip -o /work/dir/work/ZEBRA_SHARK/Pretrim_fastqc/METHYLSEQ_121923
 
 
 
