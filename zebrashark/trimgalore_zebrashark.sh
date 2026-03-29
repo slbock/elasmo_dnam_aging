285c8f189eec197566a21ddb667a86671952bb3d
samanthabock <samantha.bock@uga.edu>
2026-03-29 13:45:09 -0400

    update

--- zebrashark/trimgalore_zebrashark.sh
+++ zebrashark/trimgalore_zebrashark.sh
@@ -9,19 +9,19 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=24G
 #SBATCH --time=48:00:00
-#SBATCH --output=/scratch/sb61937/scripts/GN22898_trim_zebrashark_1.0.o
-#SBATCH --error=/scratch/sb61937/scripts/GN22898_trim_zebrashark_1.0.e
+#SBATCH --output=/dir/scripts/GN22898_trim_zebrashark_1.0.o
+#SBATCH --error=/dir/scripts/GN22898_trim_zebrashark_1.0.e
 
 
 module load Trim_Galore/0.6.7-GCCcore-11.2.0
-cd /scratch/sb61937/work/ZEBRA_SHARK/RAW_READS/METHYLSEQ_121923
+cd /dir/work/ZEBRA_SHARK/RAW_READS/METHYLSEQ_121923
 
 # trim adapter sequences from emseq reads, setting strigency to 1 since we've used this in the past for methylation data
 # adding an additional trim of 4 bp based on past m-bias plots
 
 for i in GN22898_S35_L005 GN22898_S35_L007 GN22898_S4_L004 GN22898_S4_L006
 do
-  trim_galore --fastqc --fastqc_args "--outdir /scratch/sb61937/work/ZEBRA_SHARK/TrimGalore_Fastqc/METHYLSEQ_121923 -f fastq" -stringency 1 --cores 8 --clip_r1 4 --clip_r2 4 --three_prime_clip_r1 4 --three_prime_clip_r2 4 -o /scratch/sb61937/work/ZEBRA_SHARK/TrimGalore_Fastqc/METHYLSEQ_121923 --keep --paired --retain_unpaired /scratch/sb61937/work/ZEBRA_SHARK/RAW_READS/METHYLSEQ_121923/${i}_R1_001.fastq.gz /scratch/sb61937/work/ZEBRA_SHARK/RAW_READS/METHYLSEQ_121923/${i}_R2_001.fastq.gz
+  trim_galore --fastqc --fastqc_args "--outdir /dir/work/ZEBRA_SHARK/TrimGalore_Fastqc/METHYLSEQ_121923 -f fastq" -stringency 1 --cores 8 --clip_r1 4 --clip_r2 4 --three_prime_clip_r1 4 --three_prime_clip_r2 4 -o /dir/work/ZEBRA_SHARK/TrimGalore_Fastqc/METHYLSEQ_121923 --keep --paired --retain_unpaired /dir/work/ZEBRA_SHARK/RAW_READS/METHYLSEQ_121923/${i}_R1_001.fastq.gz /dir/work/ZEBRA_SHARK/RAW_READS/METHYLSEQ_121923/${i}_R2_001.fastq.gz
 done
 
 
@@ -29,6 +29,6 @@ done
 #module load FastQC/0.11.9-Java-11
 #module load MultiQC/1.14-foss-2022a
 ## go to output directory
-#cd /scratch/sb61937/work/ZEBRA_SHARK/TrimGalore_Fastqc/METHYLSEQ_120523
+#cd /dir/work/ZEBRA_SHARK/TrimGalore_Fastqc/METHYLSEQ_120523
 ## run multiqc on the output files from fastqc
-#multiqc -d *_fastqc.zip -o /scratch/sb61937/work/ZEBRA_SHARK/TrimGalore_Fastqc/METHYLSEQ_120523
+#multiqc -d *_fastqc.zip -o /dir/work/ZEBRA_SHARK/TrimGalore_Fastqc/METHYLSEQ_120523
